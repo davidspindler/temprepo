3678332410503200585d89a00ac82d371ba66743
David <davidspindler612@gmail.com>
2022-01-31 13:43:35 -0800

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,8 @@
+// Modules
+
+const { john, peter } = require("./names");
+const sayHi = require("./utils");
+
+const data = require("./6-alternative");
+
+require("./7-mindgrenade");
---
+++ names.js
@@ -0,0 +1,10 @@
+//local
+
+const secret = "Super SecRET";
+//share
+const john = "john";
+const peter = "peter";
+
+console.log(module);
+
+module.exports = { john, peter };
